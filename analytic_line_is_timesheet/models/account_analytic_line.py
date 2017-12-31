9691fce25886182bab83aff81379b8e14b972e2e
Yasmine El Mrini <yasmine.elmrini@savoirfairelinux.com>
2017-12-30 22:17:37 -0500

    Fix compute_sheet

--- analytic_line_is_timesheet/models/account_analytic_line.py
+++ analytic_line_is_timesheet/models/account_analytic_line.py
@@ -42,8 +42,15 @@ class AccountAnalyticLine(models.Model):
         'sheet_id_computed.date_to', 'sheet_id_computed.date_from',
         'sheet_id_computed.employee_id', 'is_timesheet')
     def _compute_sheet(self):
-        timesheets = self.filtered(lambda l: l.is_timesheet)
-        super(AccountAnalyticLine, timesheets)._compute_sheet()
+        for line in self.filtered(lambda l: l.is_timesheet):
+            if not line.project_id:
+                continue
+            sheets = self.env['hr_timesheet_sheet.sheet'].search(
+                [('date_to', '>=', line.date), ('date_from', '<=', line.date),
+                 ('employee_id.user_id.id', '=', line.user_id.id)])
+            if sheets:
+                line.sheet_id_computed = sheets[0]
+                line.sheet_id = sheets[0]
 
         for line in self.filtered(lambda l: not l.is_timesheet):
             line.sheet_id = None
